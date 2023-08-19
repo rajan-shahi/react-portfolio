a155d222cd630ebf4fc3c2eb96e1e54736fbf50e
rajan <rajanshahi1267@gmail.com>
2023-08-19 02:28:29 -0700

    add aboutpage

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,8 +4,29 @@ import "./about.css";
 export default function About() {
   return (
     <div className="about">
-      <div className="left">thid is a left part</div>
-      <div className="right">this is a right part</div>
+      <div className="left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+        <img
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="right">
+      <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+      </div>
     </div>
   );
 }
